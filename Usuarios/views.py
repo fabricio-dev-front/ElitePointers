031764dbfc4b21b4e517b18ef59a9deec7a847b3
LucasMairon <lucasmairon2@gmail.com>
2024-06-10 16:50:32 -0300

    modificando cadastro e adicionando login e logout

--- Usuarios/views.py
+++ Usuarios/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render, redirect
 from .forms import UserForm
+from django.contrib.auth import login as login_django,logout as logout_django,authenticate
+from custom_user.models import User
+from .models import Usuario
+from django.contrib.auth.decorators import login_required
+
 
 def cadastrar(request):
     form = UserForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect('home')
-    
+    if request.method == "POST":
+        if form.is_valid():
+            nome = request.POST.get('nome')
+            email = request.POST.get('email')
+            senha = request.POST.get('senha')
+            user = User.objects.create_user(first_name=nome,email=email,password=senha)
+            if user:
+                usuario = Usuario()
+                usuario = form.save(commit=False)
+                usuario.user = user
+                usuario.save()
+                return redirect('home')
     return render(request,'cadastro.html',{"form":form})
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        user = authenticate(request,email=email,password=senha)
+        if user:
+            login_django(request,user)
+            return redirect('home')
+        return redirect('login')
+    
+    return render(request,'login.html')
+
+@login_required
+def logout(request):
+    logout_django(request)
+    return redirect('home')
