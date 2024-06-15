e4ec49f1af5570b07944d7e88449158b8a28c72d
LucasMairon <lucasmairon2@gmail.com>
2024-06-14 22:31:53 -0300

    adicionando funcionalidades de relogio e modificando atributos

--- Relogios/models.py
+++ Relogios/models.py
@@ -5,10 +5,32 @@ class Relogio(models.Model):
     cor = models.CharField(max_length=10)
     marca = models.CharField(max_length=30)
     imagem = models.ImageField(upload_to="Relogios_fotos")
-    genero = models.CharField(max_length=1)
+    genero = models.CharField(
+        max_length=1,
+        choices = (
+            ('M','Masculino'),
+            ('F','Feminino'),
+        ),
+    )
     preco = models.DecimalField(max_digits=10,decimal_places=2)
-    material = models.CharField(max_length=20)
-    peso = models.CharField(max_length=20)
+    material = models.CharField(max_length=20,blank=True,null=True)
+    peso = models.FloatField(
+        max_length=4,
+        blank=True,
+        null=True,
+        choices=(
+            ('1','1'),
+            ('10','10'),
+            ('12','12'), 
+            ('14','14'), 
+            ('16','16'), 
+            ('18','18'), 
+            ('19.2','19.2'), 
+            ('20','20'), 
+            ('22','22'), 
+            ('24','24'),
+        )
+    )
 
     def __str__(self):
         return self.nome
--- Relogios/views.py
+++ Relogios/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import RelogioForm
+from .models import Relogio
 
-# Create your views here.
+def cadastrarRelogio(request):
+    form = RelogioForm()
+    if request.method == "POST":
+        if form.is_valid:
+            form.save()
+    
+    return render(request,'cadastroRelogio.html',{'form': form})
+
+def listRelogios(request):
+    relogios_masculinos = Relogio.objects.get(genero='M')
+    relogios_femininos = Relogio.objects.get(genero='F')
+    return redirect('home',{'relogios_masculino':relogios_masculinos},{'relogios_femininos':relogios_femininos})
+
