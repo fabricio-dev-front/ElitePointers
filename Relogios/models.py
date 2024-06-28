2339fd3335ccbde7d5726f14341b44aff1e447ca
LucasMairon <lucasmairon2@gmail.com>
2024-06-28 15:25:34 -0300

    Adicionando funcionalidades de relogio e modificando model

--- Relogios/models.py
+++ Relogios/models.py
@@ -1,35 +1,36 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class Relogio(models.Model):
+    class Meta:
+        verbose_name = 'Relógio'
+        verbose_name_plural = 'Relógios'
+
     nome = models.CharField(max_length=20)
     cor = models.CharField(max_length=10)
     marca = models.CharField(max_length=30)
-    imagem = models.ImageField(upload_to="Relogios_fotos")
+    imagem = models.ImageField(upload_to="Relogios/fotos")
     genero = models.CharField(
-        max_length=1,
+        max_length=9,
         choices = (
-            ('M','Masculino'),
-            ('F','Feminino'),
+            ('Masculino','Masculino'),
+            ('Feminino','Feminino'),
         ),
     )
     preco = models.DecimalField(max_digits=10,decimal_places=2)
     material = models.CharField(max_length=20,blank=True,null=True)
     quantidadeDisponivel = models.IntegerField()
-    peso = models.FloatField(
-        max_length=4,
-        blank=True,
-        null=True,
+    peso = models.SmallIntegerField(
         choices=(
-            ('1','1'),
-            ('10','10'),
-            ('12','12'), 
-            ('14','14'), 
-            ('16','16'), 
-            ('18','18'), 
-            ('19.2','19.2'), 
-            ('20','20'), 
-            ('22','22'), 
-            ('24','24'),
+            (1, '1 kilate'),
+            (10, '10 kilates'),
+            (12, '12 kilates'),
+            (14, '14 kilates'),
+            (16, '16 kilates'),
+            (18, '18 kilates'),
+            (20, '20 kilates'),
+            (22, '22 kilates'),
+            (24, '24 kilates'),
         )
     )
 
--- Relogios/views.py
+++ Relogios/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import RelogioForm
 from .models import Relogio
-from Usuarios.models import Usuario,Carrinho
 from django.db.models import Q
 
 def cadastrar(request):
@@ -12,30 +11,11 @@ def cadastrar(request):
     
     return render(request,'cadastroRelogio.html',{'form': form})
 
-def listar(request):
-    relogios_masculinos = Relogio.objects.get(genero='M')
-    relogios_femininos = Relogio.objects.get(genero='F')
-    return redirect('home',{'relogios_masculino':relogios_masculinos},{'relogios_femininos':relogios_femininos})
-
 def filtrar(request,busca):
     relogios = Relogio.objects.filter(
         Q(nome__contains=busca) | Q(cor__contains=busca) | Q(marca__contains=busca) | Q(material__contains=busca)
     )
     return redirect('page',{'relogios':relogios})
-
-def adiciona_no_carrinho(request,email,pk,quantidade):
-    carrinho = Usuario.objects.get(email=email).carrinho
-    relogio = Relogio.objects.get(id=pk)
-    quantidade_disponivel = relogio.quantidadeDisponivel
-    if quantidade_disponivel >= quantidade:
-        carrinho = Carrinho()
-        carrinho.relogio = relogio
-        carrinho.quantidade = quantidade
-        carrinho.save()
-        relogio.quantidadeDisponivel = quantidade_disponivel - quantidade
-        relogio.save()
-
-    return redirect('home')
 
 
 
